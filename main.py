fa1f840df99e06d9b8af62e8c02d327d845e7400
cloud <ermuunsanaa7@gmail.com>
2025-10-01 21:37:16 +0200

    prod

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
         data = json.load(json_file)
 
     with open("data.csv", "w") as csv_file:
-        # Write CSV header
+        
         headers = data[0].keys()
         csv_file.write(",".join(headers) + "\n")
 
-        # Write CSV rows
+        
         for entry in data:
             row = [str(entry[header]) for header in headers]
             csv_file.write(",".join(row) + "\n")
